d0c608bd2592cffb8947ec65a7a55ad1da3121d8
karmaveer sharma <44526007+karm1311@users.noreply.github.com>
2019-05-02 16:32:19 +0530

    Add files via upload

---
+++ MLR from scratch and using scikit.py
@@ -0,0 +1,141 @@
+
+# coding: utf-8
+
+# In[2]:
+
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+
+# In[15]:
+
+
+data = pd.read_csv('D:\DATA SCIENCE\python\ML\potential-enigma-master/student.csv')
+data.head()
+
+math = data['Math'].values
+read = data['Reading'].values
+write = data['Writing'].values
+
+# Ploting the scores as scatter plot
+fig = plt.figure()
+ax = Axes3D(fig)
+ax.scatter(math, read, write, color='#ef1234')
+plt.show()
+
+m = len(math)
+x0 = np.ones(m)
+X = np.array([x0, math, read]).T
+# Initial Coefficients
+B = np.array([0, 0, 0])
+Y = np.array(write)
+alpha = 0.0001
+
+def cost_function(X, Y, B):
+    m = len(Y)
+    J = np.sum((X.dot(B) - Y) ** 2)/(2 * m)
+    return J
+
+inital_cost = cost_function(X, Y, B)
+print("Initial Cost")
+print(inital_cost)
+
+def gradient_descent(X, Y, B, alpha, iterations):
+    cost_history = [0] * iterations
+    m = len(Y)
+    
+    for iteration in range(iterations):
+        # Hypothesis Values
+        h = X.dot(B)
+        # Difference b/w Hypothesis and Actual Y
+        loss = h - Y
+        # Gradient Calculation
+        gradient = X.T.dot(loss) / m
+        # Changing Values of B using Gradient
+        B = B - alpha * gradient
+        # New Cost Value
+        cost = cost_function(X, Y, B)
+        cost_history[iteration] = cost
+        
+    return B, cost_history
+
+# 100000 Iterations
+newB, cost_history = gradient_descent(X, Y, B, alpha, 100000)
+
+# New Values of B
+print("New Coefficients")
+print(newB)
+
+# Final Cost of new B
+print("Final Cost")
+print(cost_history[-1])
+
+# Model Evaluation - RMSE
+def rmse(Y, Y_pred):
+    rmse = np.sqrt(sum((Y - Y_pred) ** 2) / len(Y))
+    return rmse
+
+# Model Evaluation - R2 Score
+def r2_score(Y, Y_pred):
+    mean_y = np.mean(Y)
+    ss_tot = sum((Y - mean_y) ** 2)
+    ss_res = sum((Y - Y_pred) ** 2)
+    r2 = 1 - (ss_res / ss_tot)
+    return r2
+
+Y_pred = X.dot(newB)
+
+print("RMSE")
+print(rmse(Y, Y_pred))
+print("R2 Score")
+print(r2_score(Y, Y_pred))
+
+
+# # USING SCIKIT
+
+# In[16]:
+
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+
+data = pd.read_csv('D:\DATA SCIENCE\python\ML\potential-enigma-master/student.csv')
+print(data.shape)
+data.head()
+
+math = data['Math'].values
+read = data['Reading'].values
+write = data['Writing'].values
+
+# Ploting the scores as scatter plot
+fig = plt.figure()
+ax = Axes3D(fig)
+ax.scatter(math, read, write, color='#ef1234')
+plt.show()
+
+X = np.array([math, read]).T
+Y = np.array(write)
+
+# Model Intialization
+reg = LinearRegression()
+# Data Fitting
+reg = reg.fit(X, Y)
+# Y Prediction
+Y_pred = reg.predict(X)
+
+# Model Evaluation
+rmse = np.sqrt(mean_squared_error(Y, Y_pred))
+r2 = reg.score(X, Y)
+
+print("RMSE")
+print(rmse)
+print("R2 Score")
+print(r2)
+
